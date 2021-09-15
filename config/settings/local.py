81d63aaf6307efcb00c934ee5b85c605daf8b074
Dmitry <boxdima1@gmail.com>
2021-09-15 13:59:41 -0400

    Project created

---
+++ config/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
